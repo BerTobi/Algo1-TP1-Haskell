e025f18d3200615cc7d95d0d8050c947b563aa3c
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2023-05-17 14:02:42 -0300

    Corrección de mis tests
    Mejorado el orden de las relaciones para los casos de tests

--- Solucion.hs
+++ Solucion.hs
@@ -42,7 +42,7 @@ likesDePublicacion (_, _, us) = us
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios red = eliminarRepetidos (construccionListaNombres (usuarios red))
 
-
+-- aux de nombresDeUsuarios: Construye una lista de nombres de usuarios, contiene repeticiones.
 construccionListaNombres :: [Usuario] -> [String]
 construccionListaNombres [] = []
 construccionListaNombres (x:xs) | length (x:xs) == 1 = [snd x]
@@ -249,16 +249,12 @@ sonDeLaRed red (x:xs) | length (x:xs) == 1 && pertenece (x) (usuarios red) = Tru
 -- PREDICADOS DEPENDIENTES A BASICOS
 
 --a)
-redSocialValida :: [Usuario] -> [Relacion] -> [Publicacion] -> Bool
-redSocialValida us rels pubs| usuariosValidos us == True && relacionesValidas us rels == True && publicacionesValidas us pubs == True = True
-                            | otherwise = False
-
---redSocialValida :: RedSocial -> Bool
---redSocialValida red | usuariosValidos us == True && relacionesValidas us rels == True && publicacionesValidas us pubs == True = True
---                    | otherwise = False
---                where us = usuarios red
---                      rels = relaciones red
---                      pubs = publicaciones red
+redSocialValida :: RedSocial -> Bool
+redSocialValida red | usuariosValidos us == True && relacionesValidas us rels == True && publicacionesValidas us pubs == True = True
+                    | otherwise = False
+                where us = usuarios red
+                      rels = relaciones red
+                      pubs = publicaciones red
 
 --b)
 usuariosValidos :: [Usuario] -> Bool
